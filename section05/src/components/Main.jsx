2377d4a230555fe75d9b52d775d58c69f404cda5
wally <goldenale5500@gmail.com>
2024-09-11 01:39:37 +0900

    JSX 실습

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,8 +1,31 @@
+// 주의사항
+// 1. 중괄호안에는 자바스크립트 표현식만 가능. if, for 불가능
+// 2. 숫자, 문자열, 배열깂만 렌더링 가능. boolean, undefined, null, 객체 불가능
+// 3. 모든 태근 닫혀야함
+// 4. 최상위 태그는 하나만
+
+import "./Main.css";
+
 const Main = () => {
+  const user = {
+    name: "김빵빵",
+    isLogin: true,
+  };
+
+  //   if (user.isLogin) {
+  //     return <div>로그아웃</div>;
+  //   } else {
+  //     return <div> 로그인</div>;
+  //   }
+
   return (
-    <main>
-      <h1>main</h1>
-    </main>
+    <>
+      {user.isLogin ? (
+        <div className="logout">로그아웃</div>
+      ) : (
+        <div>로그인</div>
+      )}
+    </>
   );
 };
 
