e13243fe2fe1e4bd1b3213fb8ed42ef13f0000da
wally <goldenale5500@gmail.com>
2024-10-02 17:22:28 +0900

    투루 렌더링

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -44,7 +44,7 @@ function App() {
     <>
       <Header />
       <Editor onCreate={onCreate} />
-      <List />
+      <List todos={todos} />
     </>
   );
 }
--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,38 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>TODO List</h4>
-      <input placeholder="검색어를 입력해주세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력해주세요"
+      />
       <div className="todos_warpper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />; // 반복문 할때는 반드시 key 추가
+        })}
       </div>
     </div>
   );
