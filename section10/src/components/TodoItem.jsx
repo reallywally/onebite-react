713001653e8327ff4c8129e6c51bc0efee42fe4b
reallywally <goldenale5500@gmail.com>
2024-10-08 00:40:07 +0900

    memo 렌더링 최적화

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   const onChangeCheckBox = () => {
@@ -19,4 +20,12 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+// 고차 컴포넌트(HOC)
+export default memo(TodoItem, (prevProps, nextProps) =>{
+  if(prevProps.id !== nextProps.id) return false;
+  if(prevProps.isDone !== nextProps.isDone) return false;
+  if(prevProps.content !== nextProps.content) return false;
+  if(prevProps.date !== nextProps.date) return false;
+
+  return true
+});
