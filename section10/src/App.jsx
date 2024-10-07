9458a6baea3f444d4462328d94dd6cd7c865c5dd
reallywally <goldenale5500@gmail.com>
2024-10-08 00:49:43 +0900

    useCallback

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useReducer } from "react";
+import { useRef, useState, useReducer, useCallback } from "react";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
@@ -44,7 +44,7 @@ function App() {
   const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
-  const onCreate = (content) => {
+  const onCreate =  useCallback((content) => {
     dispatch({
       type: "CREATE",
       data: {
@@ -56,21 +56,21 @@ function App() {
     });
 
     // setTodos([newTodo, ...todos]); //todos.push() 노노
-  };
+  }, []);
 
-  const onUpdate = (targetId) => {
+  const onUpdate =  useCallback((targetId) => {
     dispatch({
       type: "UPDATE",
       targetId: targetId,
     });
-  };
+  }, []);
 
-  const onDelete = (targetId) => {
+  const onDelete =  useCallback((targetId) => {
     dispatch({
       type: "DELETE",
       targetId: targetId,
     });
-  };
+  }, [])
 
   return (
     <>
