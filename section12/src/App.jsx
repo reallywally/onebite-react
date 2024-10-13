97f6a28d7276afc68ce82515b4c0c97619c6a221
reallywally <goldenale5500@gmail.com>
2024-10-14 02:10:09 +0900

    라우트 이동 방법

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,18 +1,34 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import Notfound from "./pages/Notfound";
 
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<New />} />
-      <Route path="/dirary" element={<Diary />} />
-      <Route path="*" element={<Notfound />} />
-    </Routes>
+    <>
+      <div>
+        <Link to={"/"}>Memo</Link>
+        <Link to={"/new"}>new</Link>
+        <Link to={"/dirary"}>dirary</Link>
+      </div>
+
+      <button onClick={onClickButton}>new button</button>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/dirary" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
   );
 }
 
