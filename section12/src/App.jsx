73eee27352a416e1420d78f393a9a2777fcf32ec
reallywally <goldenale5500@gmail.com>
2024-10-16 01:40:13 +0900

    Edit 추가

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,51 +1,41 @@
 import "./App.css";
+import { useReducer } from "react";
 import { Route, Routes, Link, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 
-import { getEmotionImage } from "./util/get-emotion-image";
-import Button from "./commponents/Button";
-import Header from "./commponents/Header";
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "asdasd",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "asasdasdasddasd",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-
-      <Button
-        text={"aaa"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("aaa");
-        }}
-      />
-      <div>
-        <img src={getEmotionImage(1)} />
-      </div>
-      <div>
-        <Link to={"/"}>Memo</Link>
-        <Link to={"/new"}>new</Link>
-        <Link to={"/dirary"}>dirary</Link>
-      </div>
-
-      <button onClick={onClickButton}>new button</button>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/dirary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
---
+++ section12/src/pages/Edit.jsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+const Edit = () => {
+  const params = useParams();
+
+  return <div>Edit</div>;
+};
+
+export default Edit;
